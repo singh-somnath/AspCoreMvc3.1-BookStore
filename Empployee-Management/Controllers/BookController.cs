123cbbf3a1e442218c2c1820c7e1ef0038c96ced
somnath singh <somnath@pakur2.onmicrosoft.com>
2022-10-28 21:24:35 +0530

    Created Book Controller

---
+++ Empployee-Management/Controllers/BookController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Empployee_Management.Controllers
+{
+    public class BookController : Controller
+    {
+        public string GetAllBooks()
+        {
+            return "All Books";
+        }
+         public string GetBook(int id)
+        {
+            return $"Book with id - {id}";
+        }
+
+        public string SearchBook(string bookName, string authorName)
+        {
+            return $"Book witk Name = {bookName} and Author={authorName}";
+        }
+    }
+}
