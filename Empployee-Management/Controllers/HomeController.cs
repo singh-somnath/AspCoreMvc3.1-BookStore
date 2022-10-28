460752612d05f317487f0d37d3df053b5f2fc7d7
somnath singh <somnath@pakur2.onmicrosoft.com>
2022-10-28 17:33:19 +0530

    First Commit

---
+++ Empployee-Management/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Empployee_Management.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Employee - Management";
+        }
+    }
+}
